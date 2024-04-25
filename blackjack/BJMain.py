e01609e5af6b1f9e137ad3484daf1a152f200b30
h <archood2@gmail.com>
2024-04-25 16:00:35 -0700

    FIXED THE BUGS THAT I MADE WHOOPS

--- blackjack/BJMain.py
+++ blackjack/BJMain.py
@@ -22,7 +22,6 @@ def get_score(hand):
 def play_game():
     os.system("color 2")
     print("Welcome to Blackjack with ASCII Art Cards!")
-    deck = create_deck()
     player_chips = 100
     while True:
         print(f"You have {player_chips} Copper.")
@@ -41,6 +40,7 @@ def play_game():
                 bet = 0
         if bet == 0:
             break
+        deck = create_deck()
         player_hand = [deck.pop(), deck.pop()]
         dealer_hand = [deck.pop(), deck.pop()]
         showncard = dealer_hand[0]
@@ -66,24 +66,21 @@ def play_game():
                 try:
                     player_hand.append(deck.pop())
                     print(f"\nDealer's Hand:\n{show_card(dealer_hand[0])}\n\nYour Hand:\n{show_hand(player_hand)}\n\nYour Hand's Value:{get_score(player_hand)}")
-                except IndexError:
-                    print("Error: Deck is empty. Creating a new deck.")
-                    deck = create_deck()
-                    player_hand.append(deck.pop())
+                except IndexError as e:
+                    print(f"Error: Deck is empty. Report this: \ndealer_hand:{dealer_hand}\nplayer_hand:{player_hand}\n{e}")
                     break
             else:
-                while dealer_score < 17 or (dealer_score >= 17 and any(card["Value"] == "11" for card in dealer_hand)) or (dealer_score == 21 and any(card["Rank"] == "A" for card in dealer_hand) and any(card["Value"] == "10" for card in dealer_hand) and len(dealer_hand) == 2): #some bullshit
+                while dealer_score < 17 or (dealer_score >= 17 and dealer_score == (sum(card["Value"] for card in dealer_hand) + 10)) or (dealer_score == 21 and any(card["Rank"] == "A" for card in dealer_hand) and any(card["Value"] == "10" for card in dealer_hand) and len(dealer_hand) == 2): #some bullshit
                     # Try to deal with IndexError
                     try:
                         dealer_hand.append(deck.pop())
                         dealer_score = get_score(dealer_hand)
-                    except IndexError:
-                        print("Error: Deck is empty. Creating a new deck.")
-                        deck = create_deck()
-                        player_hand.append(deck.pop())
+                    except IndexError as e:
+                        print(f"Error: Deck is empty. Report this: \ndealer_hand:{dealer_hand}\nplayer_hand:{player_hand}\n{e}")
                         break
                 print(f"\nDealer's Hand:\n{show_hand(dealer_hand)}\nDealer's Hand Value:\n{get_score(dealer_hand)}")
                 if player_score == 21 and len(player_hand) == 2:
+                    print("Blackjack!")
                     if dealer_score == 21 and len(dealer_hand) == 2:
                         pass
                 if dealer_score > 21:
